ecf2bd0bd618b738576b4aa04f138b7f873caaa0
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-05-02 18:35:09 +0200

    Added simple FB login library

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ci_facebook
 Code Igniter Facebook Login implementation
+
+This is a simple Codeigniter 3.x Facebook PHP SDK 4 implementation
+
+Just copy the facebook folder of this repository to your Codeigniter's libraries folder (application/libraries)
+
+and add to your config.php file the following variables (application/config/config.php)
+
+$config['facebook']['api_id'] = 'APP_ID';
+$config['facebook']['app_secret'] = 'APP_SECRET';
+$config['facebook']['redirect_url'] = 'REDIRECT_URL';
+$config['facebook']['permissions'] = 'public_profile, email, publish_actions';
---
+++ facebook/facebook.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+    
+    if (!defined('BASEPATH')){
+        exit('No direct script access allowed');
+    }
+    
+    // Autoload Facebook library
+    require_once(APPPATH . 'libraries/facebook/autoload.php');
+    
+    use Facebook\FacebookSession;
+	use Facebook\FacebookRedirectLoginHelper;
+	use Facebook\FacebookRequest;
+	use Facebook\FacebookResponse;
+	use Facebook\FacebookSDKException;
+	use Facebook\FacebookRequestException;
+	use Facebook\FacebookAuthorizationException;
+	use Facebook\GraphObject;
+	use Facebook\GraphUser;
+	use Facebook\GraphSessionInfo;
+    
+    class Facebook {
+        var $ci;
+        var $helper;
+        var $session;
+        var $permissions;
+    
+        public function __construct(){
+            $this->ci =& get_instance();
+            $this->permissions = $this->ci->config->item('permissions', 'facebook');
+            
+            
+            FacebookSession::setDefaultApplication($this->ci->config->item('api_id', 'facebook'), 
+                $this->ci->config->item('app_secret', 'facebook'));
+            
+            $this->helper = new FacebookRedirectLoginHelper($this->ci->config->item('redirect_url', 'facebook'));
+            $this->session = $this->helper->getSessionFromRedirect();
+            
+            if ($this->session){
+                $this->ci->session->set_userdata('fb_token', $this->session->getToken());
+            } else{
+                if ($this->ci->session->userdata('fb_token')){
+                    $this->session = new FacebookSession($this->ci->session->userdata('fb_token'));
+                    
+                    try{
+                        $this->session->validate($this->ci->config->item('api_id', 'facebook'), 
+                            $this->ci->config->item('app_secret', 'facebook'));
+                    }catch(FacebookAuthorizationException $e){
+                        echo $e->getMessage();
+                    }
+                }
+            }
+        }
+        
+        public function getSession(){
+            return $this->session;
+        }
+        
+        public function getFBLink(){
+            return $this->helper->getLoginUrl(array('scope' => $this->permissions));
+        }
+    }
+?>
